7879fb700b52e65333944e4ac609204145113b5b
然则 <hualei.hl@alibaba-inc.com>
2016-02-17 17:00:40 +0800

    autocommit

---
+++ src/main/java/json/Student.java
@@ -0,0 +1,30 @@
+package json;
+
+/**
+ * Created by hua on 16/2/17.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + "," + "name=" + name + "]";
+    }
+}
