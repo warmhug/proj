fe2304e52741bf6733a3b95700a566c984709881
warmhug <hualei5280@gmail.com>
2024-08-16 17:20:02 +0800

    try lerna

--- scripts/check-lerna.mjs
+++ scripts/check-lerna.mjs
@@ -1,8 +1,29 @@
 import parser from 'yargs-parser';
 import chalk from 'chalk';
 import { join } from 'path';
-import exec from './exec';
+import { spawn } from 'child_process';
 
+function exec(command, args, opts) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, {
+      shell: true,
+      stdio: 'inherit',
+      env: process.env,
+      ...opts,
+    });
+    child.once('error', (err) => {
+      console.log(err);
+      reject(err);
+    });
+    child.once('close', (code) => {
+      if (code === 1) {
+        process.exit(1);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
 await exec('npm', ['run', 'build']);
 
